8106793aab5423c1235e7423e29209dce9ab208f
Raistrike <32521260+Raistrike@users.noreply.github.com>
2017-11-06 19:46:06 +0100

    removed duplicate file encoder.py

--- importer.py
+++ importer.py
@@ -1,15 +1,19 @@
 from mypackage.pastebin import PastebinAPI
-import encoder
-import mypackage.csvstring as csvstring
+from mypackage import csvstring, encoder
 from lxml import etree
+
 
 def import_data():
     api_key = 'bdb21db2a07cb713b7e6a1713257564d'
     x = PastebinAPI()
-    user_key = x.generate_user_key(api_key,'Raistrike','Nuvoletta2').decode('utf-8')
-    paste = x.pastes_by_user(api_key,user_key).decode('utf-8')
+    user_key = x.generate_user_key(api_key, 'Raistrike', 'Nuvoletta2').decode('utf-8')
+    paste = x.pastes_by_user(api_key, user_key).decode('utf-8')
     root = etree.fromstring(paste)
     paste_key = root.xpath('paste_key')[0].text
-    raw = x.raw(api_key,user_key,paste_key).decode('utf-8')
-    decoded = encoder.decode(raw,return_type='str')
+    raw = x.raw(api_key, user_key, paste_key).decode('utf-8')
+    decoded = encoder.decode(raw)
+    print(decoded)
     return csvstring.csvstring_to_values(decoded)
+
+
+import_data()
--- parser.py
+++ parser.py
@@ -1,18 +1,22 @@
 import importer
-from mypackage.parser import parser
+from mypackage.parser import Parser
+
 
 def get_pastebin():
     return importer.import_data()
 
-def remove_duplicate(list):
+
+def remove_duplicate(_list):
     temp_list = []
-    for x in list:
+    for x in _list:
         if x not in temp_list:
             temp_list.append(x)
     return temp_list
 
+
 raw_data = remove_duplicate(get_pastebin())
-data = [{'title_volume':x[0],'subtitle':x[1],'publisher':x[5],'release_date':x[2],'price':x[3],'cover':x[4]} for x in raw_data]
-p = parser()
+data = [{'title_volume': x[0], 'subtitle': x[1], 'publisher': x[5], 'release_date': x[2], 'price': x[3], 'cover': x[4]}
+        for x in raw_data]
+p = Parser()
 p.parseall(data)
 p.close()
